0d8914938e1fb0bbeacce0ed9436cd1ce3788e02
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-10 11:47:21 +0100

    basicAuth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 """ Basic Authentication Model """
 
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Defining an empty BasicAuth class. """
+
+    def __init__(self) -> None:
+        """ Inintializes the parent class Auth """
+        super().__init__()
