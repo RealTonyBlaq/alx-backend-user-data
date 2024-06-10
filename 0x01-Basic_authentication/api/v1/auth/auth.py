182bdd9fe0d4fc11236e0e2a44b780b2a780acad
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-10 07:28:05 +0100

    auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,10 +2,11 @@
 """ Auth Module """
 
 from flask import request
-from typing
+from typing import List
 
 
 class Auth:
     """ Defining the class Auth """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Requires the """
