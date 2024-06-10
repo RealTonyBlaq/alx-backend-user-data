8690456b72ef2a0206e8b24d0d200dad40f53e55
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-10 17:28:04 +0100

    update

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -77,7 +77,8 @@ class BasicAuth(Auth):
             the class method search of the User to lookup the list of
             users based on their email. Don't forget to test all cases:
             “what if there is no user in DB?”, etc.
-        Return None if user_pwd is not the password of the User instance found - you must use the method is_valid_password of User
+        Return None if user_pwd is not the password of the User instance found -
+        you must use the method is_valid_password of User
         Otherwise, return the User instance
         """
         if all([user_email, type(user_email) is str, user_pwd, type(user_pwd) is str]):
