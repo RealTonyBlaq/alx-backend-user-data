5c9859c79183a902b1edf24a722a94b1a8b19f6b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-10 23:10:08 +0100

    updated passwords to include :

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -65,7 +65,7 @@ class BasicAuth(Auth):
         if decoded_base64_authorization_header and type(
               decoded_base64_authorization_header) is str:
             if ':' in decoded_base64_authorization_header:
-                user, password = decoded_base64_authorization_header.split(':')
+                user, password = decoded_base64_authorization_header.split(':', 1)
                 return (user, password)
         return (None, None)
 
