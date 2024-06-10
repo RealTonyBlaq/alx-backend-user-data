61916a5c858f739e863d35b574a75500eae9bdb9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-10 11:10:17 +0100

    update

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -26,7 +26,8 @@ def before_request():
         if auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']) is False:
             if not auth.authorization_header(request):
                 abort(401)
-            if not auth.current_user
+            if not auth.current_user(request):
+                abort(403)
 
 
 @app.errorhandler(404)
