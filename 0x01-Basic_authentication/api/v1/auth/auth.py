d7d8db050a96493d526f1c6f29374680e7fd9482
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-10 07:28:48 +0100

    auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,4 +9,7 @@ class Auth:
     """ Defining the class Auth """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Requires the """
+        """ Requires authentication for the path """
+        pass
+
+    def 
