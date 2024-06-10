56ca233a1a68eed82ca513ef18a6ee672cbe2f05
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-10 07:43:00 +0100

    update: require_auth()

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,7 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Requires authentication for the path """
         if path and excluded_paths and excluded_paths != []:
-            
+            if path in 
 
     def authorization_header(self, request=None) -> str:
         """ Retrieves auth from the Header """
