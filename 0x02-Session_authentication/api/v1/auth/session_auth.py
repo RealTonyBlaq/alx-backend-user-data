2e99eb813c8e3fe1b9b6a98ed1bd7f08effc1cd1
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-11 23:08:02 +0100

    fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,5 +25,4 @@ class SessionAuth(Auth):
             session_id = str(uuid.uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
-
-    
+        return None
