635cbef13a85f8dbae4a2c89ce5b7ba0838a8e6a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-11 22:56:40 +0100

    update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,6 +6,7 @@ from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     """ Defining the class SessionAuth that authenticates a session """
+    user_id_by_session_id
 
     def __init__(self) -> None:
         """ Initializes the inherited attributes """
