651e46237e48a167339a6954325b81aff53bc945
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-13 22:59:59 +0100

    update: find_user_by()

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm
 from user import Base, User
-from typing import TypeVar
 
 
 class DB:
