cba4639b4102dd9eeffe5e3340ce8a575b542078
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-13 21:52:58 +0100

    user class

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,6 +5,11 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
+Base = declarative_base()
+
+
 class User:
     """ Defining the class User """
     __tablename__ = "users"
+    id = Column()
+    
