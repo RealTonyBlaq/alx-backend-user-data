ea1c086d47b0fdf8242d848ac5d6dcb94a96ee4f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-13 23:25:32 +0100

    update: find_user_by()

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -43,7 +43,7 @@ class DB:
         """ Finds and returns a user object from the database """
         key = next(iter(kwargs))
         if key in ['email', 'id', 'session_id']:
-            user = self._session.query(User).where(f'{}')
+            user = self._session.query(User).
             if user:
                 return user
             raise NoResultFound()
