3eef5076b3c2de714e0cdc4fbce2f4e75a61414e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-13 23:57:09 +0100

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -61,4 +61,7 @@ class DB:
                 return None
 
             for key, value in kwargs.items():
-                if hasattr(user, key)
+                if hasattr(user, key):
+                    setattr(user, key, value)
+                else:
+                    raise ValueError()
