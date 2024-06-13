ecf947369cfb85674a8218d99cd08e960638431f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-13 22:36:14 +0100

    fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,7 +11,7 @@ Base = declarative_base()
 class User(Base):
     """ Defining the class User """
     __tablename__ = "users"
-    id = Column('id', Integer, primary_key=True)
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
     email = Column('email', String(250), nullable=False)
     hashed_password = Column('hashed_password', String(250), nullable=False)
     session_id = Column('session_id', String(250), nullable=True)
