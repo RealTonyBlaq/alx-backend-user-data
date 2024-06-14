6eeda7cb0f9fb67a4b5df070cc07ff2a0ec0bb68
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-14 20:56:07 +0100

    adding register_user()

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,7 +24,7 @@ class Auth:
         """ Initializes the _db attribute """
         self._db = DB()
 
-    def register_user(self, email: str, pwd: str) -> TypeVar('User'):
+    def register_user(self, email: str, pwd: str) -> User:
         """ Registers a user if the email doesn't exist in the db. """
         if all([email, pwd, type(email) is str, type(pwd) is str]):
             try:
