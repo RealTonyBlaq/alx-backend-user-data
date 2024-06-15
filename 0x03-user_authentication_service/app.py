b4b2154f2361e4000e71a19d8d406ae39d7cfd5c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-15 21:38:59 +0100

    flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,4 +7,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes)
+@app.route('/', strict_slashes=False)
+def home() -> str:
+    """ Returns the home JSON page """
+    return jsonify()
