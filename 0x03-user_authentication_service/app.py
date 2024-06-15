ae22061ef620faba401159e397ba882df1d7c44f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-15 21:39:43 +0100

    flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,4 +10,8 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def home() -> str:
     """ Returns the home JSON page """
-    return jsonify()
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
