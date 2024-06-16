241a70b4a4b30bfe9d5765865beef0b0bbb325e4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-16 17:20:27 +0100

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -102,4 +102,5 @@ class Auth:
         if reset_token and password:
             try:
                 user = self._db.find_user_by(reset_token=reset_token)
-                self._db.update_user(user.id, )
+                password = _hash_password
+                self._db.update_user(user.id, reset_token=None, hashed_password=)
