6e765b3b2867f16fec19e15b171a8408613a6ca4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-16 15:45:24 +0100

    app update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -30,5 +30,8 @@ def users() -> str:
             return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
