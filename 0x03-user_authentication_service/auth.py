026adb9e3eecc9b97032c549bdf28b1db67a8fe6
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-16 16:10:28 +0100

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,4 +70,6 @@ class Auth:
             try:
                 user = self._db.find_user_by(session_id=session_id)
                 return user
-            except NoResultFound
+            except (NoResultFound, InvalidRequestError):
+                pass
+        return None
