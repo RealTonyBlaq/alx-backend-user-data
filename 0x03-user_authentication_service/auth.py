bbf5daff0691d0715eeea98a68f0feec47b8cc86
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-16 16:17:53 +0100

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,5 +79,6 @@ class Auth:
         if user_id and type(user_id) is int:
             try:
                 user = self._db.find_user_by(id=user_id)
-                self._db.update_user(user_id, session_id="")
+                self._db.update_user(user_id, session_id=None)
+            except (NoResultFound, )
             
