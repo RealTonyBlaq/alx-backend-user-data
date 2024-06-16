7bbcf27fee7fd7bf3b2e669b712257e6bc126446
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-16 15:36:28 +0100

    validates a login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,4 +59,4 @@ class Auth:
         if email:
             try:
                 user = self._db.find_user_by(email=email)
-                self._db.update_user(user.id, {})
+                self._db.update_user(user.id, {'session_id': _generate_uuid()})
