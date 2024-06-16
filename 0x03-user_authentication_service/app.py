550c3519150b5f46ef51218d1aff32b41e7a1bb3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-16 16:29:35 +0100

    update app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,6 +57,8 @@ def new_session() -> str:
         cookie = request.cookies.get('session_id')
         if cookie:
             user = AUTH.get_user_from_session_id(cookie)
+            if user:
+                AUTH.destroy_session(user.id)
             
 
 
