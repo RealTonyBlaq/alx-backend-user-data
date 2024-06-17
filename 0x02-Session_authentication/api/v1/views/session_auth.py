a62ae1eaf786bbb78bc4c90d3795a8291c4d9cc5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-17 22:20:30 +0100

    update: new route for DELETE /logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -44,4 +44,5 @@ def safe_login() -> str:
 @app_views.route('/api/v1/auth_session/logout', methods=['DELETE'],
                  strict_slashes=False)
 def safe_logout() -> str:
-    """ Dele"""
+    """ Deletes a User session and logs them out """
+    cookie = request.cookies.get('')
