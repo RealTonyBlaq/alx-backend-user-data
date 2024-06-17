2e09b191512c8cd43275dbbddc7f901537265ce9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-17 21:07:32 +0100

    testing the app

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -7,7 +7,7 @@ import requests
 def register_user(email: str, password: str) -> None:
     """ Tests POST /users endpoint """
     r = requests.post('http://127.0.0.1:5000/users',
-                      params={'email': email, 'password': password})
+                      data={'email': email, 'password': password})
     assert r.json() == {"email": email, "message": "user created"}
     assert r.status_code == 200
 
@@ -15,14 +15,14 @@ def register_user(email: str, password: str) -> None:
 def log_in_wrong_password(email: str, password: str) -> None:
     """ Tests POST /sessions with wrong login details """
     r = requests.post('http://127.0.0.1:5000/sessions',
-                      params={'email': email, 'password': password})
+                      data={'email': email, 'password': password})
     assert r.status_code == 401
 
 
 def log_in(email: str, password: str) -> str:
     """ Tests POST /sessions with the correct login details """
     r = requests.post('http://127.0.0.1:5000/sessions',
-                      params)
+                      data={})
 profile_unlogged() -> None
 profile_logged(session_id: str) -> None
 log_out(session_id: str) -> None
