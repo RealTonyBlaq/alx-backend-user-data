83afde06d1c351748b6ddd0a1d18d4561bbc36fe
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-17 21:58:54 +0100

    update: destroy_session()

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -40,3 +40,6 @@ class SessionAuth(Auth):
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """ """
