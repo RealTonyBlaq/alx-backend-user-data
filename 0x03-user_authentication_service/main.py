8c1d701d99551dd14e721053a56d449481fe40dc
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-17 21:15:14 +0100

    testing the app

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -34,7 +34,8 @@ def profile_unlogged() -> None:
 
 
 def profile_logged(session_id: str) -> None:
-    """ Tests GET /profile with a session_id"""
+    """ Tests GET /profile with a session_id """
+    r = requests.get('http://')
 log_out(session_id: str) -> None
 reset_password_token(email: str) -> str
 update_password(email: str, reset_token: str, new_password: str) -> None
