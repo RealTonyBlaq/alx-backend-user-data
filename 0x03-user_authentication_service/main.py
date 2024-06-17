15fb0e0009d269446655510cfafde325bb918a79
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-17 21:09:02 +0100

    testing the app

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -22,7 +22,8 @@ def log_in_wrong_password(email: str, password: str) -> None:
 def log_in(email: str, password: str) -> str:
     """ Tests POST /sessions with the correct login details """
     r = requests.post('http://127.0.0.1:5000/sessions',
-                      data={'email': email, })
+                      data={'email': email, 'password': password})
+    assert r.json() == 
 profile_unlogged() -> None
 profile_logged(session_id: str) -> None
 log_out(session_id: str) -> None
