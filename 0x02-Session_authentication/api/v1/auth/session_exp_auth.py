d374bcf7ed5f603fabae6a9aebd92bc18c0d62ac
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-18 21:29:29 +0100

    session expiry

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -32,5 +32,5 @@ class SessionExpAuth(SessionAuth):
             if user_session:
                 if self.session_duration <= 0:
                     return user_session.get('user_id')
-                if self
+                if user_session.get('created_at'):
         return None
