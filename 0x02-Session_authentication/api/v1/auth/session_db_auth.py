b782a4225683918ccdce8f3a76ff1b5c45806df7
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-18 23:18:13 +0100

    new AUth

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -32,4 +32,4 @@ class SessionDBAuth(SessionExpAuth):
         if request:
             session_id = self.session_cookie(request)
             if session_id:
-                users = 
+                users = UserSession.search({session_id'})
