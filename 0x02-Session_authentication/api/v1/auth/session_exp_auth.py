31f3503d4323e1fc5baccbf594bd1edb92a5ce72
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-18 20:46:31 +0100

    session

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -7,4 +7,6 @@ from session_auth import SessionAuth
 class SessionExpAuth(SessionAuth):
     """ Defining the SessionExpAuth model """
 
-    
+    def __init__(self) -> None:
+        """ Initializing the attributes """
+        super().__init__()
