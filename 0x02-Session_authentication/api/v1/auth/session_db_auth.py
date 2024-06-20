b9e00476d0d701cb0408ffe322aef1e9a2ce85f3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-20 06:29:17 +0100

    fix

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ SessionDBAuth Module """
 
+from os import getenv
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from datetime import datetime, timedelta
 from models.user_session import UserSession
 
 
@@ -20,7 +22,20 @@ class SessionDBAuth(SessionExpAuth):
 
     def user_id_for_session_id(self, session_id=None) -> str:
         """ Returns the user_id that matches a session_id """
-        return super().user_id_for_session_id(session_id)
+        if session_id:
+            users = UserSession.search({'session_id': session_id})
+            if users is None or users == []:
+                return None
+
+            user = users[0]
+            duration = getenv('SESSION_DURATION')
+            current_time = datetime.now()
+
+            if duration is None or duration <= 0:
+                return user.user_id
+            if current_time > user.created_at + timedelta(seconds=duration):
+                return None
+            return user.user_id
 
     def destroy_session(self, request=None) -> None:
         """ Deletes a user session """
@@ -28,6 +43,6 @@ class SessionDBAuth(SessionExpAuth):
             session_id = self.session_cookie(request)
             if session_id:
                 users = UserSession.search({'session_id': session_id})
-                for user in users:
-                    user.remove()
+                if users and users != []:
+                    users[0].remove()
         return None
