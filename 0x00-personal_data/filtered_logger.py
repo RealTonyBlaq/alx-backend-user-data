1f94ee6a915f712a66a8e3f09acf041c54c28ce6
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-23 16:16:59 +0100

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,5 +19,5 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         fields in the log line (message)
     """
     for field in fields:
-        re.sub(fr'({field}=)[^{separator}]+({separator})', fr'\1{redaction}\2', message)
-    return message
+        new_message = re.sub(fr'({field}=)[^{separator}]+({separator})', fr'\1{redaction}\2', message)
+    return new_message
