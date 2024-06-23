fd402001f9ede4f7bd3114b4fd135e984547fe1d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-23 16:39:14 +0100

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,8 +5,10 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, sp: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ Returns the log message obfuscated """
     for field in fields:
-        message = re.sub(fr'({field}=)[^{sp}]+({sp})', fr'\1{redaction}\2', message)
+        message = re.sub(fr'({field}=)[^{separator}]+({separator})',
+                         fr'\1{redaction}\2', message)
     return message
