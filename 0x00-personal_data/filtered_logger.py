fda23b41e67162305d4e075289eaa6aaf876a4a1
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-23 16:32:24 +0100

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,5 +7,5 @@ import re
 
 def filter_datum(fields: List[str], rplc: str, msg: str, sp: str) -> str:
     """ Returns the log message obfuscated """
-    return re.sub(f'({"|".join(fields)})=[^;]*', f'\\1={rplc}', message)
-
+    sp = re.escape(sp)
+    return re.sub(f'({"|".join(fields)})=[^{sp}]*', f'\\1={rplc}', msg)
