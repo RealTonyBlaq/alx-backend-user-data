3629f3cdd9de1a64dcc1ab315ffc4e06caa6e404
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-23 15:51:47 +0100

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,12 +6,15 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
     """
-    function called filter_datum that returns the log message obfuscated:
+    Returns the log message obfuscated
 
     Arguments:
+    ----------
 
     fields: a list of strings representing all fields to obfuscate
     redaction: a string representing by what the field will be obfuscated
     message: a string representing the log line
-    separator: a string representing by which character is separating all fields in the log line (message)
+    separator: a string representing by which character is separating all
+        fields in the log line (message)
     """
+    
